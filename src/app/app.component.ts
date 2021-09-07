322aae6774e09f6aa9da78d6502bd95fc94fe661
florian935 <florian.martin63000@gmail.com>
2021-09-07 17:13:43 +0200

    Feat: added animation for heroes routing slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { slideInAnimation } from './animation';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    animations: [slideInAnimation],
 })
 export class AppComponent {
+    getAnimationData(outlet: RouterOutlet): string {
+        return (
+            outlet &&
+            outlet.activatedRouteData &&
+            outlet.activatedRouteData.animation
+        );
+    }
 }
